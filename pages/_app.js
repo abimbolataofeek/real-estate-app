e5bc73f79a70c8e4801d762585f4b9f1e8a7bdb0
abimbolataofeek <abimbolaadewale9999@gmail.com>
2021-12-25 16:35:38 +0100

    properties added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'Nprogress';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import {Flex, Box, Text, Button } from '@chakra-ui/react';
 import {baseUrl, fetchApi} from '../utils/fetchApi';
+import Property from '../components/Property';
 
 const Banner =({purpose, imageUrl, title1,title2, desc1, desc2, linkName, buttonText}) => (
   <Flex flexWrap= "wrap " justifyContent="center" alignItems="center" m="10">
@@ -18,8 +19,6 @@ const Banner =({purpose, imageUrl, title1,title2, desc1, desc2, linkName, button
 ) 
 
 export default function Home({propertiesForSale, propertiesForRent}) {
-console.log(propertiesForSale, propertiesForRent);
- 
   return (
     <Box >
       <h1>Hello Next.js</h1>
@@ -34,19 +33,21 @@ console.log(propertiesForSale, propertiesForRent);
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"    
       />
       <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
           
       </Flex>
 
       <Banner 
-        purpose="BUY A HOME"
+        purpose="BUY A HOME"    
         title1="Find, Buy & Own Your"
         title2="Dream Home"
         desc1= "Explore Apartments, Villas, Homes"
         desc2= "and more..."
         buttonText= "Explore Renting"
         linkName= "/search?purpose=for-rent"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Box>
   )
 }
